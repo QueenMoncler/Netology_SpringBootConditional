c3ed8418af718c6ce8cc6771d50289689a841faa
Queen <QueenMoncler@gmail.com>
2023-10-28 13:56:37 +0300

    Add and Change

---
+++ src/main/java/profile/SystemProfile.java
@@ -0,0 +1,5 @@
+package profile;
+
+public interface SystemProfile {
+    String getProfile();
+}
--- src/main/java/ru/netology/netology_springbootconditional/NetologySpringBootConditionalApplication.java
+++ src/main/java/ru/netology/netology_springbootconditional/NetologySpringBootConditionalApplication.java
@@ -3,11 +3,15 @@ package ru.netology.netology_springbootconditional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RestController;
+
+
 @SpringBootApplication
 public class NetologySpringBootConditionalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NetologySpringBootConditionalApplication.class, args);
 	}
+
 
 }
